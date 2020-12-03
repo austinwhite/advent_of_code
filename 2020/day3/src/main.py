f2f97e0e8dfb34b0857aebff88c1079baf3fb2f4
Austin White <austin@austinwhite.com>
2020-12-02 23:20:10 -0700

    day3

--- 2020/day3/src/main.py
+++ 2020/day3/src/main.py
@@ -14,18 +14,18 @@ def trees_hit(mountain, move_x, move_y):
 
     return trees_hit
 
-def trees_hit_multiple_inputs(mountain, inputs):
+def trees_hit_multiple_slopes(mountain, slopes):
     total_trees = 1
-    for element in inputs:
-        total_trees *= trees_hit(mountain, element[0], element[1])
+    for slope in slopes:
+        total_trees *= trees_hit(mountain, slope[0], slope[1])
     return total_trees
 
 mountain = []
-inputs = [[1,1], [3,1], [5,1], [7,1], [1,2]]
+slopes = [[1,1], [3,1], [5,1], [7,1], [1,2]]
 
 with open("../input/input.txt") as file:
     for line in file:
         mountain.append([char for char in line][:-1])
 
 print("Trees hit:", trees_hit(mountain, 3, 1))
-print("Trees hit:", trees_hit_multiple_inputs(mountain, inputs))
+print("Trees hit:", trees_hit_multiple_slopes(mountain, slopes))
